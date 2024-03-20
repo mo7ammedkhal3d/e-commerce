033aa50dce474ba486340c7e9b10a1f16af50ffc
Mo'Khaled Bin-Hazem <Mo7ammedkhal3d@gmail.com>
2024-03-20 23:54:42 +0300

    init commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+     echo "<h1>Hello Mohammed khaled how are you to day are you ok let us make somthing great and start up our power to tell them what can we do in our plate form </h1>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/main.css">
+    
+    <title>E-commerce</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="notification">
+
+            </div>
+            <div class="searash-section">
+
+            </div>
+            <div class="navigation-links">
+
+            </div>
+            <div class="logo">
+
+            </div>
+        </nav>
+    </header>
+    <main></main>
+    <footer></footer>
+</body>
+</html>
