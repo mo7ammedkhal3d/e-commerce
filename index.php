c3fc377862664035c0992be711132522a295885f
Mo'Khaled Bin-Hazem <Mo7ammedkhal3d@gmail.com>
2024-03-21 00:06:27 +0300

    add fontawosme kit and reorder linkes

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-     echo "<h1>Hello Mohammed khaled how are you to day are you ok let us make somthing great and start up our power to tell them what can we do in our plate form </h1>";
+    //  echo "<h1>Hello Mohammed khaled how are you to day are you ok let us make somthing great and start up our power to tell them what can we do in our plate form </h1>";
 ?>
 
 <!DOCTYPE html>
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/css/main.css">
-    
+    <script src="https://kit.fontawesome.com/16f6ba35a2.js" crossorigin="anonymous"></script>
     <title>E-commerce</title>
 </head>
 <body>
