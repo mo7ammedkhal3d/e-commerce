f8f1bc477010e7ac857e4a9c9e68b444e6f67219
Mo'Khaled Bin-Hazem <Mo7ammedkhal3d@gmail.com>
2024-03-22 16:59:26 +0300

    Start in archtiture of designe

--- index.php
+++ index.php
@@ -1,34 +1,54 @@
 <?php
-     echo "<h1>Hello Mohammed khaled how are you to day are you ok let us make somthing great and start up our power to tell them what can we do in our plate form </h1>";
+//  echo "<h1>Hello Mohammed khaled how are you to day are you ok let us make somthing great and start up our power to tell them what can we do in our plate form </h1>";
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Links -->
     <link rel="stylesheet" href="./assets/css/main.css">
-    
+    <script src="https://kit.fontawesome.com/16f6ba35a2.js" crossorigin="anonymous"></script>
+
     <title>E-commerce</title>
 </head>
+
 <body>
     <header>
         <nav>
-            <div class="notification">
-
-            </div>
-            <div class="searash-section">
-
+            <div class="logo">
+                <img src="" alt="">
             </div>
             <div class="navigation-links">
-
+                <ul>
+                    <li>Home</li>
+                    <li>Categories</li>
+                    <li>Services</li>
+                    <li>Trend</li>
+                    <li>Views</li>
+                </ul>
             </div>
-            <div class="logo">
-
+            <div class="searash-section">
+                <input type="text">
+                <i class="fa-solid fa-magnifying-glass searsh-icon"></i>
             </div>
+            <div class="notification">
+                <div class="messages"><i class="fa-regular fa-envelope notification-icon"></i></div>
+                <div class="notifications"><i class="fa-regular fa-bell notification-icon"></i></div>
+                <div class="cart"><i class="fa-solid fa-cart-plus notification-icon"></i></div>
+            </div>
+            <div class="login"></div>
         </nav>
     </header>
-    <main></main>
-    <footer></footer>
+    <main>
+
+    </main>
+    <footer>
+        
+    </footer>
 </body>
+
 </html>
